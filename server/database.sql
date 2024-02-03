032d19fec5660894f12428609270747d6a39a76f
RH-Kevin <147596439+RH-Kevin@users.noreply.github.com>
2024-02-03 21:47:28 +0900

    add: seed data for DB

--- server/database.sql
+++ server/database.sql
@@ -4,3 +4,8 @@ CREATE TABLE IF NOT EXISTS todo (
     todo_id SERIAL PRIMARY KEY,
     description VARCHAR(255)
 );
+
+INSERT INTO todo (description) VALUES
+    ('Sample Todo 1'),
+    ('Sample Todo 2'),
+    ('Sample Todo 3');
