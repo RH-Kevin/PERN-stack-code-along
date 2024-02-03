9165c7bf0d522a1efe89938a340283c201728854
RH-Kevin <147596439+RH-Kevin@users.noreply.github.com>
2024-02-03 20:23:31 +0900

    issue: DBconfig.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const dotenv = require('dotenv');
-const poolItems = require('./dbconfig');
+const poolItems = require('./DBconfig.js')
 
 
 dotenv.config();
