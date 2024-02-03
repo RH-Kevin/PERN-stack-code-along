7e9a8b85ea824b061281eac04c107943fd07fc73
RH-Kevin <147596439+RH-Kevin@users.noreply.github.com>
2024-02-03 18:26:28 +0900

    Config: Change to DBconfig.js

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
-const Pool = require("pg").Pool;
+// const Pool = require("pg").Pool;
 
-const pool = new Pool({
-    user: "kevinhaith",
-    password: "",
-    host: "localhost",
-    port: 5432,
-    database: "perntodo"
-});
+// const pool = new Pool({
+//     user: "kevinhaith",
+//     password: "",
+//     host: "localhost",
+//     port: 5432,
+//     database: "perntodo"
+// });
 
-module.exports = pool;
+// module.exports = pool;
