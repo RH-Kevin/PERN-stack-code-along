f23703f292bfec328813d8175db5af082618858e
RH-Kevin <147596439+RH-Kevin@users.noreply.github.com>
2024-02-03 21:57:24 +0900

    config: DB URL

--- server/DBconfig.js
+++ server/DBconfig.js
@@ -4,14 +4,15 @@ dotenv.config();
 
 const { Pool } = require('pg');
 const poolItems = new Pool({
-    connectionString: process.env.DBConfigLink,
-    ssl: {
-        rejectUnauthorized: false
-    },
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    // connectionString: process.env.DBConfigLink,
+    // ssl: {
+    //     rejectUnauthorized: false
+    // },
+    // user: process.env.DB_USER,
+    // host: process.env.DB_HOST,
+    // database: process.env.DB_NAME,
+    // password: process.env.DB_PASSWORD,
+    // port: process.env.DB_PORT,
+    connection: process.env.DATABASE_URL,
 });
 module.exports = poolItems;
