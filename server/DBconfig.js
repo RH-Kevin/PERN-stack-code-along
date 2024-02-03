797e582a8a126fe5d62916b58d47b9e4d339b611
RH-Kevin <147596439+RH-Kevin@users.noreply.github.com>
2024-02-03 21:39:20 +0900

    add: DB query configs

--- server/DBconfig.js
+++ server/DBconfig.js
@@ -7,6 +7,11 @@ const poolItems = new Pool({
     connectionString: process.env.DBConfigLink,
     ssl: {
         rejectUnauthorized: false
-    }
+    },
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 module.exports = poolItems;
